b74d8a3b6b09e5a1c716c815ae4aa4ccd1471553
matiashoz <matiashoz22@gmail.com>
2021-09-02 14:43:19 -0300

    seccion 1917

---
+++ claseHoz.js
@@ -0,0 +1,12 @@
+
+let nombreIngresado = prompt("Ingrese su nombre y apellido"); 
+let edadIngresada = prompt("Ingrese su edad");
+
+if (nombreIngresado = "Garcia"){
+	alert("Bienvenido a la familía García");
+}
+
+if (edadIngresada > 18){
+	alert("Podrás ingresar a las películas con clasificación R")
+}
+
